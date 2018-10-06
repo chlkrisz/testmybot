95ef0c57a38408c0b297f43f3eee574fd5b3878d
chlkrisz <39501471+chlkrisz@users.noreply.github.com>
2018-10-06 13:30:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,26 +4,8 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`subscribe`);
 });
-
-client.on("guildCreate", guild => {
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
-client.on("guildDelete", guild => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
-});
-
-function altgen() {
-  var rand = ['user:155',
-  'asgasg:123',
-  '15132:sfasf'];
-
-  return rand[Math.floor(Math.random()*rand.length)];
-}
 
 client.on("message", async message => {
 
@@ -35,6 +17,7 @@ client.on("message", async message => {
   const command = args.shift().toLowerCase();
 
 if(command === "test") {
+  message.delete().catch();
   message.channel.send("Test!");
 
   }
